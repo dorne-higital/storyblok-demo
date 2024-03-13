ebb7ec5617c7ae91962c0fd6e5121e6bb71f65ee
Dan <dan.horne@nexuspoint.co.uk>
2024-03-13 21:08:41 +0000

    init

---
+++ nuxt.config.js
@@ -0,0 +1,15 @@
+export default defineNuxtConfig({
+  css: ['@/assets/css/roboto.css'],
+  modules: [
+    [
+      '@storyblok/nuxt',
+      {
+        accessToken: '7Z515hA2EeT6KjX3U7HOJgtt',
+        apiOptions: {
+          region: '' // Set 'US" if your space is created in US region (EU default)
+        }
+      },
+    ],
+    '@nuxtjs/tailwindcss',
+  ]
+})
