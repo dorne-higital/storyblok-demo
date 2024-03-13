2ea4421b33a6c479a63707dd3b640802ad553184
Dan <dan.horne@nexuspoint.co.uk>
2024-03-13 21:44:39 +0000

    Create Hero.vue

---
+++ storyblok/Hero.vue
@@ -0,0 +1,23 @@
+<template>
+    <div
+      v-editable="blok"
+      class="w-full p-12 bg-[#f7f6fd] rounded-[5px] text-center"
+    >
+      <h2 class="text-2xl text-[#1d243d] font-bold">
+        {{ blok.heading }}
+      </h2>
+
+      <h5 class="text-2xl text-[#1d243d] font-bold">
+        {{ blok.subheading }}
+      </h5>
+  
+      <p>
+        {{ blok.description }}
+      </p>
+    </div>
+  </template>
+  
+  <script setup>
+  defineProps({ blok: Object });
+  </script>
+  
